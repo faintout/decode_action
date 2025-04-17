978f52ba090c0010cfed72258ba20f8c18a91153
action <action@github.com>
2025-04-17 09:57:55 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Jan 03 2025 05:59:42 GMT+0000 (Coordinated Universal Time)
+//Thu Apr 17 2025 09:57:55 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const _0xea70a1 = _0x1bd4ec("农夫山泉2024"),
